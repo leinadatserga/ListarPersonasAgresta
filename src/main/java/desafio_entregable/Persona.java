9f9fd40c1eb9390c04dfa1151c4ba70113219966
Daniel Agresta <leinadatserga@gmail.com>
2024-06-05 12:27:13 -0300

    Desafio entregable ListarPersonas

---
+++ src/main/java/desafio_entregable/Persona.java
@@ -0,0 +1,29 @@
+public class Persona {
+    String nombre;
+    String apellido;
+
+    public Persona (){};
+
+    public Persona(String apellido, String nombre) {
+        this.apellido = apellido;
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+
+}
